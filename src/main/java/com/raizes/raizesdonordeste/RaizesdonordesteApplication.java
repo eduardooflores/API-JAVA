579748bd7c23c68efe24be09b43903901d760d35
Eduardo Flores <eduardofloresdev@gmail.com>
2026-08-09 13:32:35 -0300

    chore: estrutura inicial do projeto Spring Boot via Initializr

---
+++ src/main/java/com/raizes/raizesdonordeste/RaizesdonordesteApplication.java
@@ -0,0 +1,13 @@
+package com.raizes.raizesdonordeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesdonordesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizesdonordesteApplication.class, args);
+	}
+
+}
