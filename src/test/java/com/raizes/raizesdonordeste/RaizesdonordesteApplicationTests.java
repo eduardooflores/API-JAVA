46b257d2db6e02d4da14a578e1bf7845c64510ce
Eduardo Flores <eduardofloresdev@gmail.com>
2026-08-09 13:34:21 -0300

    chore: adiciona arquivos de configuracao do maven wrapper e estrutura de testes

---
+++ src/test/java/com/raizes/raizesdonordeste/RaizesdonordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raizes.raizesdonordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesdonordesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
