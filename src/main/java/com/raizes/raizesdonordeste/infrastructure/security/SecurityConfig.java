00d1e3c76643f566c3509c60c050a69a2fb78523
Eduardo Flores <eduardofloresdev@gmail.com>
2026-08-10 20:08:42 -0300

    feat: adiciona Swagger/OpenAPI, libera rotas de documentacao no SecurityConfig

--- src/main/java/com/raizes/raizesdonordeste/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/raizes/raizesdonordeste/infrastructure/security/SecurityConfig.java
@@ -40,7 +40,11 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .requestMatchers(
                                 HttpMethod.GET,
                                 "/produtos/**",
